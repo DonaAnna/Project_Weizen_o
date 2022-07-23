d2e628190c1714a39e30fedb9f223e2ee636abb5
DonaAnna <donaannavarghese@mca.ajce.in>
2022-07-23 21:05:06 +0530

    committed

---
+++ src/components/Slider.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import ArrowLeftOutlinedIcon from '@mui/icons-material/ArrowLeftOutlined';
+import ArrowRightOutlinedIcon from '@mui/icons-material/ArrowRightOutlined';
+import styled from 'styled-components'
+
+const Container = styled.div`
+width: 100%;
+height:100vh;
+display:flex;
+background-color:coral;
+position:relative;
+`
+const Arrow = styled.div`
+width: 50px;
+height: 50px;
+border-radius:50%;
+background-color:#F5FEFD;
+display:flex;
+align-items:center;
+justify-content: center;
+position:absolute;
+top:0;
+bottom:0;
+left:${props=> props.direction === "left" && "10px"};
+right:${props=> props.direction === "right" && "10px"};
+
+margin:auto;
+`
+const Slider = () => {
+  return (
+    <Container>
+        <Arrow direction="left">
+        <ArrowLeftOutlinedIcon/>
+        </Arrow>
+        <Arrow direction="right">
+        <ArrowRightOutlinedIcon/>
+        </Arrow>
+    </Container>
+  )
+}
+
+export default Slider
