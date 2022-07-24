f4a94228074d4abe94bbd48d08216e267d3d5e98
DonaAnna <donaannavarghese@mca.ajce.in>
2022-07-24 17:05:23 +0530

    committed

---
+++ src/data.js
@@ -0,0 +1,59 @@
+export const sliderItems = [
+    {
+        id:1,
+        img:"https://thumbs.dreamstime.com/b/golden-wheat-isolated-white-background-33793503.jpg",
+        title:"FARM FRESH WHEAT",
+        desc:"CHERISH YOUR FAMILY WITH FRESH WHEAT",
+        bg:"#ffffff"
+    },
+    {
+        id:2,
+        img:"https://thumbs.dreamstime.com/b/wheat-ears-6051234.jpg",
+        title:"FARM FRESH WHEAT",
+        desc:"CHERISH YOUR FAMILY WITH FRESH WHEAT",
+        bg:"#ffffff"
+    },
+    
+    {
+        id:3,
+        img:"https://www.biogreenscience.com/img/Spaghetti-Pasta-Macaroni-Soup-Noodles-Durum-Wheat.jpg",
+        title:"FARM FRESH WHEAT",
+        desc:"CHERISH YOUR FAMILY WITH FRESH WHEAT",
+        bg:"#f9fafa"
+    },
+];
+
+export const categories = [
+{
+    id:1,
+    img:"https://5.imimg.com/data5/ANDROID/Default/2022/3/FA/UJ/VW/97745743/fb-img-1641737058550-jpg-1000x1000.jpg",
+    title:"EMMER WHEAT",
+},
+{
+    id:2,
+    img:"https://th.bing.com/th/id/OIP.dzCcCeNBSgfouDwchi8JFQHaE8?pid=ImgDet&rs=1",
+    title:"RED WINTER WHEAT",
+},
+{
+    id:3,
+    img:"https://5.imimg.com/data5/ANDROID/Default/2021/12/JU/XT/ZB/22903060/product-jpeg-250x250.jpg",
+    title:"BLACK WHEAT",
+},
+{
+    id:4,
+    img:"https://5.imimg.com/data5/ANDROID/Default/2022/5/WA/DN/GW/82191934/product-jpeg-500x500.jpg",
+    title:"MILLING WHEAT",
+},
+{
+    id:5,
+    img:"http://5.imimg.com/data5/SELLER/Default/2021/12/GY/GH/LZ/130751610/wheat-500x500.jpg",
+    title:"LOKWAN WHEAT",
+},
+{
+    id:6,
+    img:"https://5.imimg.com/data5/QG/BZ/DU/SELLER-14740323/maida-flour-250x250.jpg",
+    title:"WHITE WINTER WHEAT",
+},
+
+
+];
